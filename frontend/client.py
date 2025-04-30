77832df21e1e55199a593060e151d3c877dce018
purio <wizard.bass5@gmail.com>
2025-04-30 17:34:07 +0900

    サーバーとクライアントの設定読み込み機能を追加

--- frontend/client.py
+++ frontend/client.py
@@ -2,36 +2,37 @@ import socket
 import json
 import sys
 import os
-
-with open("config.json", "r") as f:
-    config = json.load(f)
-
-server_address = config["server_address"]
-server_port = config["server_port"]
+import math
 
 def main():
+    config = load_config()
+    server_info = (config["server_address"], config["server_port"])
+
     file_path = input("Type the path of the file you want to upload: ")
     if not os.path.exists(file_path):
         print(f'File not found: {file_path}')
         sys.exit(1)
-    tcp_handler(file_path)
+    tcp_handler(file_path, server_info)
 
-def tcp_handler(file_path):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def tcp_handler(file_path, server_info):
     try:
-        sock.connect((server_address, server_port))
-        send_file(sock, file_path)
-        receive_response(sock)
+        tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp_sock.connect(server_info)
+        send_file(tcp_sock, file_path)
+        receive_response(tcp_sock)
     except socket.error as e:
         print(f"Error connecting to server: {e}")
         sys.exit(1)
     finally:
-        sock.close()
+        tcp_sock.close()
     
 def send_file(sock, file_path):
     try:
         with open(file_path, "rb") as f:
             filesize = os.path.getsize(file_path)
+            if check_filesize(filesize):
+                print("File size is too large")
+                sys.exit(1)
             header = filesize.to_bytes(32, 'big')
             sock.sendall(header)
 
@@ -51,6 +52,20 @@ def receive_response(sock):
     except socket.error as e:
         print(f'Error receiving response: {e}')
         sys.exit(1)
+
+def load_config(path="config.json"):
+    try:
+        with open(path, 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print(f'Config file not found: {path}')
+        sys.exit(1)
+    except json.JSONDecodeError:
+        print(f'Invalid JSON format in config file: {path}')
+        sys.exit(1)
+
+def check_filesize(filesize):
+    return filesize > math.pow(2, 32)
 
 if __name__ == "__main__":
     main()
